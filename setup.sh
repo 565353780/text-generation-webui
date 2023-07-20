bb27e0224b16693c7b02062375f52be590a449a4
chLi <565353780@qq.com>
2023-07-20 20:50:10 +0800

    update all install and update scripts

---
+++ setup.sh
@@ -0,0 +1,16 @@
+cd ..
+git clone https://github.com/oobabooga/text-generation-webui textgen
+
+cp ./text-generation-webui/characters/* ./textgen/characters/
+cp ./text-generation-webui/models/config-user.yaml ./textgen/models/config-user.yaml
+
+cd textgen
+git pull
+
+pip uninstall -y exllama
+pip uninstall -y llama-cpp-python
+
+pip install -r requirements.txt --upgrade
+
+pip uninstall -y llama-cpp-python
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
