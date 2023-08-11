e5dc2a22543f1a3ac7551cac4f043fb1f63d2086
chLi <565353780@qq.com>
2023-08-11 18:57:26 +0800

    update install packages

--- dev_setup.sh
+++ dev_setup.sh
@@ -13,7 +13,9 @@ pip uninstall -y llama-cpp-python
 pip install -U torch torchvision torchaudio
 pip install -r requirements.txt --upgrade
 
-pip install -U git+https://github.com/huggingface/transformers
+pip install -U git+https://github.com/huggingface/transformers.git
+pip install -U git+https://github.com/huggingface/peft.git@70af02a2bca5a63921790036b2c9430edf4037e2
+pip install -U git+https://github.com/sterlind/GPTQ-for-LLaMa.git@lora_4bit
 
 pip uninstall -y llama-cpp-python
 CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
